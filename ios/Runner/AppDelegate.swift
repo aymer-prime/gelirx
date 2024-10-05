3eb34f9ae8c86a3f712dd79b9f25cc978c1f1d72
Mostafa <mostafa@customsurgical.co>
2024-10-05 11:31:53 +0400

    fix push kit ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,149 @@
 import Flutter
 import UIKit
+import PushKit
+import CallKit
+import flutter_callkit_incoming
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, PKPushRegistryDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    UIApplication.shared.registerForRemoteNotifications()
+    let mainQueue = DispatchQueue.main
+    let voipRegistry: PKPushRegistry = PKPushRegistry(queue: mainQueue)
+    voipRegistry.delegate = self
+    voipRegistry.desiredPushTypes = [PKPushType.voIP]
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    // Call back from Recent history
+    override func application(_ application: UIApplication,
+                              continue userActivity: NSUserActivity,
+                              restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        
+        guard let handleObj = userActivity.handle else {
+            return false
+        }
+        
+        guard let isVideo = userActivity.isVideo else {
+            return false
+        }
+        let objData = handleObj.getDecryptHandle()
+        let nameCaller = objData["nameCaller"] as? String ?? ""
+        let handle = objData["handle"] as? String ?? ""
+        let data = flutter_callkit_incoming.Data(id: UUID().uuidString, nameCaller: nameCaller, handle: handle, type: isVideo ? 1 : 0)
+        //set more data...
+        //data.nameCaller = nameCaller
+        SwiftFlutterCallkitIncomingPlugin.sharedInstance?.startCall(data, fromPushKit: true)
+        
+        return super.application(application, continue: userActivity, restorationHandler: restorationHandler)
+    }
+    
+    // Handle updated push credentials
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+        print(credentials.token)
+        let deviceToken = credentials.token.map { String(format: "%02x", $0) }.joined()
+        print(deviceToken)
+        //Save deviceToken to your server
+        SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP(deviceToken)
+    }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
+        print("didInvalidatePushTokenFor")
+        SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP("")
+    }
+    
+    // Handle incoming pushes
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
+        print("didReceiveIncomingPushWith")
+        guard type == .voIP else { return }
+        let dic = payload.dictionaryPayload as NSDictionary
+        let aps = dic["aps"] as! NSDictionary
+        let id = dic["id"] as? String ?? ""
+        let nameCaller = payload.dictionaryPayload["nameCaller"] as? String ?? ""
+        let handle = payload.dictionaryPayload["handle"] as? String ?? ""
+        let isVideo = payload.dictionaryPayload["isVideo"] as? Bool ?? false
+        
+        let data = flutter_callkit_incoming.Data(id: id, nameCaller: nameCaller, handle: handle, type: isVideo ? 1 : 0)
+        //set more data
+        data.extra = ["user": "abc@123", "platform": "ios"]
+        //data.iconName = ...
+        //data.....
+        SwiftFlutterCallkitIncomingPlugin.sharedInstance?.showCallkitIncoming(data, fromPushKit: true)
+//        
+//        //Make sure call completion()
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//            completion()
+//        }
+    }
+    
+    
+    // Func Call api for Accept
+//    func onAccept(_ call: Call, _ action: CXAnswerCallAction) {
+//        let json = ["action": "ACCEPT", "data": call.data.toJSON()] as [String: Any]
+//        print("LOG: onAccept")
+//        self.performRequest(parameters: json) { result in
+//            switch result {
+//            case .success(let data):
+//                print("Received data: \(data)")
+//                //Make sure call action.fulfill() when you are done(connected WebRTC - Start counting seconds)
+//                action.fulfill()
+//
+//            case .failure(let error):
+//                print("Error: \(error.localizedDescription)")
+//            }
+//        }
+//    }
+//    
+//    // Func Call API for Decline
+//    func onDecline(_ call: Call, _ action: CXEndCallAction) {
+//        let json = ["action": "DECLINE", "data": call.data.toJSON()] as [String: Any]
+//        print("LOG: onDecline")
+//        self.performRequest(parameters: json) { result in
+//            switch result {
+//            case .success(let data):
+//                print("Received data: \(data)")
+//                //Make sure call action.fulfill() when you are done
+//                action.fulfill()
+//
+//            case .failure(let error):
+//                print("Error: \(error.localizedDescription)")
+//            }
+//        }
+//    }
+//    
+//    // Func Call API for End
+//    func onEnd(_ call: Call, _ action: CXEndCallAction) {
+//        let json = ["action": "END", "data": call.data.toJSON()] as [String: Any]
+//        print("LOG: onEnd")
+//        self.performRequest(parameters: json) { result in
+//            switch result {
+//            case .success(let data):
+//                print("Received data: \(data)")
+//                //Make sure call action.fulfill() when you are done
+//                action.fulfill()
+//
+//            case .failure(let error):
+//                print("Error: \(error.localizedDescription)")
+//            }
+//        }
+//    }
+//    
+//    // Func Call API for TimeOut
+//    func onTimeOut(_ call: Call) {
+//        let json = ["action": "TIMEOUT", "data": call.data.toJSON()] as [String: Any]
+//        print("LOG: onTimeOut")
+//        self.performRequest(parameters: json) { result in
+//            switch result {
+//            case .success(let data):
+//                print("Received data: \(data)")
+//
+//            case .failure(let error):
+//                print("Error: \(error.localizedDescription)")
+//            }
+//        }
+//    }
+
 }
--- lib/app/local_services/notifiaction_handler.dart
+++ lib/app/local_services/notifiaction_handler.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_callkit_incoming/entities/android_params.dart';
 import 'package:flutter_callkit_incoming/entities/call_event.dart';
 import 'package:flutter_callkit_incoming/entities/call_kit_params.dart';
+import 'package:flutter_callkit_incoming/entities/ios_params.dart';
 import 'package:flutter_callkit_incoming/flutter_callkit_incoming.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/v4.dart';
 import '../injector/injection.dart';
 import '../navigation/app_router.dart';
 import '../utils/app_constants.dart';
@@ -14,7 +18,18 @@ class NotificationHandler {
 
   NotificationHandler(this.messaging);
 
-  void listenToNotification() {
+  Future<void> listenToNotification() async {
+    var push =  await FlutterCallkitIncoming.getDevicePushTokenVoIP();
+    print(" push = ${push}");
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       _handleIncomingCall(message);
     });
@@ -22,13 +37,38 @@ class NotificationHandler {
 
   void _handleIncomingCall(RemoteMessage message) {
     // Show the call
-    const params = CallKitParams(
-      id: "1111",
+    var id = Uuid().v4();
+    var params = CallKitParams(
+      id: id,
       appName: "Gelirx",
       duration: 30000,
       nameCaller: "service name",
       textAccept: "take",
       textDecline: "decline",
+        android: AndroidParams(
+          isCustomNotification:
+          true, // Enables custom notification layout for Android
+          isShowLogo: true, // Show app logo in notification
+          ringtonePath: "ringtone_default", // Custom ringtone for Android
+          backgroundColor: "#E30A17", // Background color for the notification
+          backgroundUrl:
+          "https://example.com/background.jpg", // URL for background image
+          actionColor: "#4CAF50", // Color for action buttons
+        ),
+        ios: IOSParams(iconName: 'CallKitLogo',
+            handleType: 'generic',
+            supportsVideo: true,
+            maximumCallGroups: 2,
+            maximumCallsPerCallGroup: 1,
+            audioSessionMode: 'default',
+            audioSessionActive: true,
+            audioSessionPreferredSampleRate: 44100.0,
+            audioSessionPreferredIOBufferDuration: 0.005,
+            supportsDTMF: true,
+            supportsHolding: true,
+            supportsGrouping: false,
+            supportsUngrouping: false,
+            ringtonePath: 'system_ringtone_default')
     );
 
     FlutterCallkitIncoming.showCallkitIncoming(params);
