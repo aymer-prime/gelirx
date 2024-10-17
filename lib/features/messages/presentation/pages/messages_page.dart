6922f822f93468b12307877fb7846db4a2c3b0e9
NazirDan <naz1r-aldan@outlook.com>
2024-10-17 14:30:10 +0400

    order details page UI update

--- lib/features/messages/presentation/pages/messages_page.dart
+++ lib/features/messages/presentation/pages/messages_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gelirx/app/extensions/List.dart';
-import 'package:gelirx/app/extensions/context.dart';
 import 'package:gelirx/app/utils/resources/assets_manager.dart';
 import 'package:gelirx/app/utils/resources/color_manager.dart';
 import 'package:gelirx/app/utils/resources/font_manager.dart';
